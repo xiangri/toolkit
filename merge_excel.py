e2444c7a02a14c3faeb3d594ed784490dd442f15
xiangri <xiangri1124@gmail.com>
2022-01-08 21:54:37 +0800

    update

--- merge_excel.py
+++ merge_excel.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 
 import os
+from re import T
 import shutil
-import fire
-from fire.core import Fire
+from numpy.core.fromnumeric import transpose
 import openpyxl
 import pandas as pd
-from copy import deepcopy
-
+import xlrd
+import xlwings as xw
 
 def get_all_workbook_files_name(src_dir):
     workbook_files = []
@@ -19,21 +19,10 @@ def get_all_workbook_files_name(src_dir):
     return workbook_files
 
 
-def check_row(row, not_null_columns):
+def merge(src_dir, result_file='all.xlsx', not_null_columns=None,):
     """
-    全部not_null_columns列都不为空才返回True
-    :param row:
-    :param not_null_columns:
-    :return:
+    将src_dir下面的所有xlsx文件合并到一个xlsx文件中，同名的表竖着合并
     """
-    for i in not_null_columns:
-        if row[int(i)] is None:
-            return False
-
-    return True
-
-
-def merge(src_dir, not_null_columns=None, result_file='all.xlsx'):
     workbook_files = get_all_workbook_files_name(src_dir)
     not_null_columns_dic = {}
     if not_null_columns is not None:
@@ -42,55 +31,76 @@ def merge(src_dir, not_null_columns=None, result_file='all.xlsx'):
             if sheet and column:
                 not_null_columns_dic[sheet] = int(column)
 
-    wb_new = openpyxl.Workbook()
-
-    for f in workbook_files:
-        wb = openpyxl.load_workbook(f)
-        for s in wb.worksheets:
-            if s.title in wb_new.sheetnames:
-                curr_sheet = wb_new[s.title]
-            else:
-                print("load sheet ", s.title)
-                curr_sheet = wb_new.create_sheet(s.title)
-            for row in s:
-                # 如果表名在not_null_columns中，且这列的单元格值是空的就跳过这行
-                if s.title in not_null_columns_dic and row[not_null_columns_dic[s.title]].value is None:
-                    continue
-                new_row = [cell.value for cell in row]
-                curr_sheet.append(deepcopy(new_row))
-
-
-    wb_new.save(result_file)
-    print("merge done , result_file: ", result_file)
-
-
-class Main(object):
-    @staticmethod
-    def merge2one(src_dir='.', not_null_columns=None, result_file='all.xlsx'):
-        """
-        not_null_columns='银行存款:1'
-        """
-        merge(src_dir, not_null_columns, result_file)
+    new_wb = {}
     
-    @staticmethod
-    def help():
-        text = """
-        1. 遍历src_dir，把所有同名的表合并到一个工作簿的一张里。
-            --not_null_columns: 指定过滤项，不填即不过滤。比如 --not_null_columns='银行存款:1', 如果'银行存款'这张表的某一行第1(B)列的值为空就不要这一行
-                                支持对每个表设置一列过滤列，--not_null_columns='银行存款:1,银行借款:3'
-            --result_file: 结果保存到哪个文件，默认当前目录下的 all.xlsx
+    for f in workbook_files:
+        # sheet_name=None表示读取所有sheet,否则默认只读第一个sheet
+        # wb是一个dict，k是表名，v是表的值 DadaFrame类型
+        # wb = pd.read_excel(f, sheet_name=None)
+        print("load workbook ", f)
+        wb = pd.read_excel(f, sheet_name=None, dtype=object)
 
-            useage : python merge_excel.py merge2one --src_dir='/Users/xiangri/github/toolkit/excel' --not_null_columns='银行存款:1,银行借款:3'
-            easy   : python merge_excel.py merge2one '/Users/xiangri/github/toolkit/excel' '银行存款:1,银行借款:3'
-        
-        """
-        print(text)
+        for sheet_name in wb.keys():
+            if sheet_name not in new_wb:
+                new_wb[sheet_name] = []
+                print("find sheet ", sheet_name)
+            new_wb[sheet_name].append(wb[sheet_name])
+    print("load all workbook finished, start save result to ", result_file)
+    with pd.ExcelWriter(result_file) as writer:
+        for sheet_name, sheet_value_list in new_wb.items():
+            # 将多个DadaFrame竖着合并。横着合并取axis=1
+            df = pd.concat(sheet_value_list, axis=0)
+            # 将合并后的dataframe当成一张sheet写入excle文件
+            df.to_excel(writer, sheet_name=sheet_name)
+    print("merge done , please check result_file: ", result_file)
+
+def merge2(src_dir, result_file='all.xlsx', not_null_columns=None,):
+    """
+    将src_dir下面的所有xlsx文件合并到一个xlsx文件中，同名的表竖着合并
+    """
+    workbook_files = get_all_workbook_files_name(src_dir)
+    not_null_columns_dic = {}
+    if not_null_columns is not None:
+        for sheet_column in not_null_columns.split(','):
+            sheet, column = sheet_column.split(':')
+            if sheet and column:
+                not_null_columns_dic[sheet] = int(column)
+
+    new_wb = {}
+    
+    for f in workbook_files:
+        # sheet_name=None表示读取所有sheet,否则默认只读第一个sheet
+        # wb是一个dict，k是表名，v是表的值 DadaFrame类型
+        # wb = pd.read_excel(f, sheet_name=None)
+        print("load workbook ", f)
+        # wb = pd.read_excel(f, sheet_name=None, dtype=object, engine='openpyxl', formatting_info=True)
+        wb = pd.read_excel(f, sheet_name=None, dtype=object)
+
+        for sheet_name in wb.keys():
+            if sheet_name not in new_wb:
+                new_wb[sheet_name] = []
+                print("find sheet ", sheet_name)
+            new_wb[sheet_name].append(wb[sheet_name])
+    print("load all workbook finished, start save result to ", result_file)
+    # wb = xlrd.open_workbook(workbook_files[0], formatting_info=True)
+    # app = xw.App(visible=False)
+    app = xw.App(spec='wpsoffice')
+
+    app.display_alerts = False
+    app.screen_updating = False
+    # wb = xw.Book(workbook_files[0])
+    wb = app.books.open(workbook_files[0])
+    for sheet_name, sheet_value_list in new_wb.items():
+        df = pd.concat(sheet_value_list, axis=0)
+        wb.sheets[sheet_name].options(transpose=True).value = df
+    wb.save(result_file)
+    wb.close()
+    app.quit()
+    print("merge done , please check result_file: ", result_file)
 
 
 if __name__ == '__main__':
-    # fire.Fire(Main)
-    src_dir = r'c:\sss'
-    not_null_columns = None
-    # not_null_columns = '银行存款:1,银行借款:3'
-    result_file = r'all.xlsx'
-    Main.merge2one(src_dir, not_null_columns, result_file)
+    src_dir = r'/Users/xiangri/github/toolkit/excel'
+    result_file = r'all—2.xlsx'
+    # merge(src_dir, result_file)
+    merge2(src_dir, result_file)
